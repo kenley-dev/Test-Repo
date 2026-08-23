041fefacf50c844cca4b1ae472636f5ff6f10bbe
Kenley <kenley.abaka@gmail.com>
2026-08-23 19:56:49 -0400

    added is Prime feature

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ def isOddOrEven(num):
     else:
         return "Odd"
 
-num = int(input("Enter your number: "))
-result = isOddOrEven(num)
-print(f"The number {num} is {result}")
+def isPrime(num):
+    if num <= 1:
+        return "not prime"
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return "not prime"
+    return "prime"
+
+num = int(input("Enter a number: "))
+print(f"The number {num} is {isOddOrEven(num)} and its is {isPrime(num)}.")
